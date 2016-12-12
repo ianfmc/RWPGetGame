832364d4f59515cc8ed3021c2e3a73ff8bf2f29b
Ian McCallum <ianfmc@gmail.com>
2016-12-12 14:02:45 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var AWS = require('aws-sdk');
+
+exports.handler = function(event, context, callback) {
+    
+    var teamID = event.teamID.toString();
+
+    var docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+    var teamParams = {
+        TableName : 'Team',
+        Key : {
+            teamID: teamID
+        },
+    }
+    docClient.get(teamParams, function(err, data) {
+        if (err) {
+            callback(new Error('Unknown Team'));
+        }
+        else {
+            callback(null, data);
+          }
+      });
+};
